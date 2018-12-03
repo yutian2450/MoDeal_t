8df094637e0ccb80f3249167a19d1f9ef84e7cc5
唐茂志 <hat@hats-MacBook-Pro.local>
2018-12-02 19:33:22 -0800

    first commit

--- MoDeal_t/MoDeal_t/amazon/test.py
+++
@@ -1,6 +0,0 @@
-
-from amazon.api import AmazonAPI
-
-amazon=AmazonAPI("AKIAI6DA7AZ35GPPONJQ", "lngMtLZjOI+k3zrmGqXeck5cyuNJXoMnbyIhyI8Q", "hat07c-20")
-product = amazon.lookup(ItemId='B00EOE0WKQ')
-product.title
--- MoDeal_t/MoDeal_t/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/hat/PycharmProjects/MoDeal_t/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
